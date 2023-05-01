f706f157b9a02a180e63b00bd6c1ea7a5508c818
OmidSargazi <om.sargazi@gmail.com>
2023-05-01 23:45:14 +0330

    fetch data with redux toolkit nad show in post.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
+"use client";
 import Image from "next/image";
-
+import Counter from "./componente/counter";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { Fragment } from "react";
+import Post from "./componente/post";
 export default function Home() {
   return (
-    <div className="text-3xl text-center mt-10 text-amber-600 hover:text-amber-800">
-      Redux Toolkit
-    </div>
+    <Fragment>
+      <div className="text-3xl text-center mt-10 text-amber-600 hover:text-amber-800">
+        <Provider store={store}>
+          Redux Toolkit
+          <Counter />
+          <Post />
+        </Provider>
+      </div>
+    </Fragment>
   );
 }
