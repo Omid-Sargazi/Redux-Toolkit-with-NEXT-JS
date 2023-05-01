87043e2d11542ee55aba5de5c9648d82fae2df5d
OmidSargazi <om.sargazi@gmail.com>
2023-05-01 20:39:13 +0330

    create-store

---
+++ app/sliceCounter/SliceCounter.jsx
@@ -0,0 +1,27 @@
+// "use client";
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   value: 0,
+// };
+
+// const INC = (state) => {
+//   state.value += 1;
+// };
+
+// const DEC = (state) => {
+//   state.value -= 1;
+// };
+
+// const CounterSlice = createSlice({
+//   name: "count",
+//   initialState,
+//   reducers: {
+//     INC,
+//     DEC,
+//   },
+// });
+
+// export const { INC: increment, DEC: decrement } = CounterSlice.actions;
+
+// export default CounterSlice.reducer;
---
+++ app/store/store.jsx
@@ -0,0 +1,11 @@
+"use client"
+import { configureStore } from "@reduxjs/toolkit";
+
+
+const store = configureStore({
+    reducer:{
+
+    }
+})
+
+export default store;
